902737f4df89d8dc3238eac653baa22f11e8db10
Priyanka Langote <161610464+Lpriyanka25@users.noreply.github.com>
2024-11-19 23:05:50 +0530

    Update linklist.cpp

--- linklist.cpp
+++ linklist.cpp
@@ -1,3 +1,4 @@
+//example 1
 #include <iostream>
 using namespace std;
 
@@ -92,6 +93,103 @@ int main() {
 
     cout << "Linked List after deletion: ";
     list.display();
+
+    return 0;
+}
+//example 2
+#include <iostream>
+using namespace std;
+
+// Node structure
+struct Node {
+    int data;        // Data to store
+    Node* next;      // Pointer to the next node
+};
+
+// Linked List class
+class LinkedList {
+private:
+    Node* head;      // Pointer to the first node in the list
+
+public:
+    // Constructor
+    LinkedList() : head(nullptr) {}
+
+    // Function to insert at the end
+    void append(int value) {
+        Node* newNode = new Node();
+        newNode->data = value;
+        newNode->next = nullptr;
+
+        if (!head) { // If the list is empty
+            head = newNode;
+        } else {
+            Node* temp = head;
+            while (temp->next) { // Traverse to the end of the list
+                temp = temp->next;
+            }
+            temp->next = newNode;
+        }
+    }
+
+    // Function to display the linked list
+    void display() {
+        if (!head) {
+            cout << "The list is empty." << endl;
+            return;
+        }
+        Node* temp = head;
+        while (temp) {
+            cout << temp->data << " -> ";
+            temp = temp->next;
+        }
+        cout << "nullptr" << endl;
+    }
+
+    // Destructor to free memory
+    ~LinkedList() {
+        Node* temp = head;
+        while (temp) {
+            Node* next = temp->next;
+            delete temp;
+            temp = next;
+        }
+    }
+};
+
+int main() {
+    LinkedList list;
+    int choice, value;
+
+    do {
+        cout << "\nMenu:\n";
+        cout << "1. Add an element to the list\n";
+        cout << "2. Display the list\n";
+        cout << "3. Exit\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        switch (choice) {
+            case 1:
+                cout << "Enter a value to add: ";
+                cin >> value;
+                list.append(value);
+                cout << "Value added successfully!" << endl;
+                break;
+
+            case 2:
+                cout << "The current linked list is: ";
+                list.display();
+                break;
+
+            case 3:
+                cout << "Exiting program." << endl;
+                break;
+
+            default:
+                cout << "Invalid choice! Please try again." << endl;
+        }
+    } while (choice != 3);
 
     return 0;
 }
